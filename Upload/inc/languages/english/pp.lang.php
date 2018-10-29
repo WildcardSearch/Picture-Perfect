597f128d834cf08985529af02dcb93e15a21b404
Mark Vincent <admin@rantcentralforums.com>
2018-10-28 20:05:33 -0500

    Fixes #33

---
+++ Upload/inc/languages/english/pp.lang.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: Picture Perfect for MyBB 1.8.x
+ * Copyright 2018 WildcardSearch
+ * http://www.rantcentralforums.com
+ *
+ * ACP language file
+ */
+
+$l['pp'] = 'Picture Perfect';
+
+?>
