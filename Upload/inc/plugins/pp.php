bd57d708153e0a2b3417dc2fe9695cff95f37a9f
Mark Vincent <2533207+WildcardSearch@users.noreply.github.com>
2019-10-12 12:14:46 -0500

    0.0.9 Pre-release

--- Upload/inc/plugins/pp.php
+++ Upload/inc/plugins/pp.php
@@ -12,7 +12,7 @@ if (!defined('IN_MYBB')) {
     die('Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.');
 }
 
-define('PICTURE_PERFECT_VERSION', '0.0.8');
+define('PICTURE_PERFECT_VERSION', '0.0.9');
 define('PICTURE_PERFECT_MOD_URL', MYBB_ROOT.'inc/plugins/pp/modules');
 define('PICTURE_PERFECT_HOST_URL', MYBB_ROOT.'inc/plugins/pp/hosts');
 
